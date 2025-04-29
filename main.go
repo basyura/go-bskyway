a26b3089a129c896bc5f6475394707cb1864f5dc
basyura <basyura@gmail.com>
2025-04-29 11:31:31 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -22,6 +22,42 @@ func main() {
 }
 
 func doMain() error {
+	// 設定の初期化
+	if err := initialize(); err != nil {
+		return err
+	}
+
+	// セッションの生成
+	ctx := context.Background()
+	client, err := session.NewSession(ctx)
+	if err != nil {
+		return err
+	}
+
+	// 最後に通知したフィード
+	lastCid := ""
+	for {
+		// タイムラインの取得
+		out, err := bsky.FeedGetTimeline(ctx, client, "reverse-chronological", "", 10)
+		// セッションの再生成
+		if err != nil {
+			if strings.Contains(err.Error(), "ExpiredToken") {
+				client, err = session.NewSession(ctx)
+				if err != nil {
+					return fmt.Errorf("session refresh failed: %w", err)
+				}
+				continue
+			}
+		}
+
+		// 通知
+		lastCid = notify(out, lastCid)
+		// 次の取り込みまでの待機
+		time.Sleep(30 * time.Second)
+	}
+}
+
+func initialize() error {
 	fmt.Println("")
 	myFigure := figure.NewFigure("bskyway", "", true)
 	myFigure.Print()
@@ -36,51 +72,36 @@ func doMain() error {
 	fmt.Println("IconCacheDir :", config.IconCacheDir)
 	fmt.Println("")
 
-	// セッションの生成
-	ctx := context.Background()
-	client, err := session.NewSession(ctx)
-	if err != nil {
-		return err
+	return nil
+}
+
+func notify(out *bsky.FeedGetTimeline_Output, lastCid string) string {
+	// 最後に通知したフィード以降のみに絞る
+	feeds := filter(out, lastCid)
+	// 通知対象が無い
+	if len(feeds) == 0 {
+		return lastCid
 	}
 
-	lastCid := ""
-	for {
-		out, err := bsky.FeedGetTimeline(ctx, client, "reverse-chronological", "", 5)
-		if err != nil {
-			if strings.Contains(err.Error(), "ExpiredToken") {
-				client, err = session.NewSession(ctx)
-				if err != nil {
-					return fmt.Errorf("session refresh failed: %w", err)
-				}
-				continue
-			}
+	// 取得と逆順に古いものから通知
+	for i := len(feeds) - 1; i >= 0; i-- {
+		feed := feeds[i]
+		if feed.Post == nil {
+			continue
 		}
 
-		feeds := filter(out, lastCid)
-		if len(feeds) != 0 {
-			for i := len(feeds) - 1; i >= 0; i-- {
-				feed := feeds[i]
-				if feed.Post == nil {
-					continue
-				}
+		post := model.ConvertToPost(feed)
+		lastCid = post.Cid
 
-				post := model.ConvertToPost(feed)
-				if post.Cid == lastCid {
-					break
-				}
-				lastCid = post.Cid
+		// 通知
+		notifier.Notify(post)
 
-				fmt.Println(post.Format())
-				notifier.Notify(post)
-				fmt.Println("--------------------------------------------------")
-				time.Sleep(5 * time.Second)
-			}
-		}
-		// fmt.Println("==================================================")
-
-		// 次の取り込み用
-		time.Sleep(30 * time.Second)
+		fmt.Println(post.Format())
+		fmt.Println("--------------------------------------------------")
+		time.Sleep(5 * time.Second)
 	}
+
+	return lastCid
 }
 
 func filter(out *bsky.FeedGetTimeline_Output, lastCid string) []*bsky.FeedDefs_FeedViewPost {
