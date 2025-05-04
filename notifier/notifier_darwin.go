0a251cc4ce38adec07a1315df19891e57facdfe2
basyura <basyura@gmail.com>
2025-05-04 14:34:12 +0900

    リファクタリング

--- notifier/notifier_darwin.go
+++ notifier/notifier_darwin.go
@@ -3,46 +3,15 @@
 package notifier
 
 import (
-	"bskyway/config"
 	"bskyway/model"
-	"crypto/sha1"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
 	"os/exec"
-	"path/filepath"
 )
 
 func Notify(post *model.Post) {
 	title := post.Name
 	message := post.Text
-
-	avatarURL := post.Avatar
-	// Generate a deterministic filename based on the avatar URL so we can reuse it from the temp directory.
-	hash := sha1.Sum([]byte(avatarURL))
-	filename := fmt.Sprintf("%x%s", hash[:], ".jpeg")
-	cacheDir := config.Instance().IconCacheDir
-	imgPath := filepath.Join(cacheDir, filename)
-
-	// Download the avatar only if it does not already exist in the temp directory.
-	if _, err := os.Stat(imgPath); os.IsNotExist(err) {
-		resp, err := http.Get(avatarURL)
-		if err != nil {
-			fmt.Println("avatar download error:", err)
-		} else {
-			defer resp.Body.Close()
-			out, err := os.Create(imgPath)
-			if err != nil {
-				fmt.Println("avatar file create error:", err)
-			} else {
-				if _, err = io.Copy(out, resp.Body); err != nil {
-					fmt.Println("avatar save error:", err)
-				}
-				out.Close()
-			}
-		}
-	}
+	imgPath := downloadAvatar(post)
 
 	cmd := exec.Command("terminal-notifier",
 		"-title", title,
